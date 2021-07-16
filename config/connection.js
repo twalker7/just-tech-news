390d69ff4d74efe1fe5a71db7bb5a0c0fd963f4c
Thomas Walker <thomasnwalker@yahoo.com>
2021-07-15 23:37:53 -0700

    database and database connection created

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize('just_tech_news_db', "root", "777mysql", {
+    host: "localhost",
+    dialect : 'mysql',
+    port: 3306
+
+});
+
+module.exports = sequelize;
+
