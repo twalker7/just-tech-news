c2007ade52768c5dc9e9951d473c373fb2b6f0a4
Thomas Walker <thomasnwalker@yahoo.com>
2021-07-16 15:14:03 -0700

    user model initialized

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('just_tech_news_db', "root", "777mysql", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PW, {
     host: "localhost",
     dialect : 'mysql',
     port: 3306
